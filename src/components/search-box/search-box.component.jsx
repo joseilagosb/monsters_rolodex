aaac651848261a11bca0c4456f78cfc178d69448
joseilagosb <joseilagosb@gmail.com>
2021-08-10 15:54:07 -0400

    hola

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './search-box.styles.css'
+
+export const SearchBox = ({ placeholder, onSearchChange }) => {
+  return (
+    <input className="search" 
+      type="search" 
+      placeholder={placeholder}
+      onChange={onSearchChange}
+    />
+  );
+}
